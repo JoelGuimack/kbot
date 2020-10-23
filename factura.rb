654fb2a44745418e2583b3a682ce59d7647b2bfc
JoelGuimack <you@example.com>
2020-10-23 00:38:08 +0000

    Slice 5, Reorganizacion en metodos y cambio de mensaje en impuesto

--- factura.rb
+++ factura.rb
@@ -2,11 +2,18 @@ def precioTotal(cantidad, precioUnitario)
     return cantidad.to_f * precioUnitario.to_f
 end
 
+def calculoTasaImpuesto()
+    return 0.0825
+end
+
+def pintarParametros(cantidad, precioUnitario, tasaImpuestoAplicado, facturacionTotal)
+    puts "Cantidad: #{cantidad}, Precio Unitario: #{precioUnitario} = #{facturacionTotal}"
+    puts "Impuesto Aplicado: #{tasaImpuestoAplicado}"
+end
 cantidad = ARGV[0]
 precioUnitario = ARGV[1]
 facturacionTotal = precioTotal(cantidad, precioUnitario)
-tasaImpuestoAplicado = 0.0825
-puts "Cantidad: #{cantidad}, Precio Unitario: #{precioUnitario} = #{facturacionTotal}"
+tasaImpuestoAplicado = calculoTasaImpuesto()
+pintarParametros(cantidad, precioUnitario, tasaImpuestoAplicado, facturacionTotal)
 facturacionTotal += facturacionTotal*tasaImpuestoAplicado
-puts "Impuesto Fijo: #{tasaImpuestoAplicado}"
 puts "Total: #{facturacionTotal}"
