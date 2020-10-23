08f7c23ef9c8bee90c64ea0c934d42474be15987
JoelGuimack <you@example.com>
2020-10-23 00:49:54 +0000

    Slice, Reorganizacion en una clase

--- factura.rb
+++ factura.rb
@@ -1,19 +1,23 @@
-def precioTotal(cantidad, precioUnitario)
-    return cantidad.to_f * precioUnitario.to_f
-end
+class Facturacion
+    def precioTotal(cantidad, precioUnitario)
+        return cantidad.to_f * precioUnitario.to_f
+    end
 
-def calculoTasaImpuestoFijo()
-    return 0.0825
-end
+    def calculoTasaImpuestoFijo()
+        return 0.0825
+    end
 
-def pintarParametros(cantidad, precioUnitario, tasaImpuestoAplicado, facturacionTotal)
-    puts "Cantidad: #{cantidad}, Precio Unitario: #{precioUnitario} = #{facturacionTotal}"
-    puts "Impuesto Aplicado Fijo: #{tasaImpuestoAplicado}"
+    def pintarParametros(cantidad, precioUnitario, tasaImpuestoAplicado, facturacionTotal)
+        puts "Cantidad: #{cantidad}, Precio Unitario: #{precioUnitario} = #{facturacionTotal}"
+        puts "Impuesto Aplicado Fijo: #{tasaImpuestoAplicado}"
+    end
+    
 end
 cantidad = ARGV[0]
 precioUnitario = ARGV[1]
-facturacionTotal = precioTotal(cantidad, precioUnitario)
-tasaImpuestoAplicado = calculoTasaImpuestoFijo()
-pintarParametros(cantidad, precioUnitario, tasaImpuestoAplicado, facturacionTotal)
+facturacion= Facturacion.new()
+facturacionTotal = facturacion.precioTotal(cantidad, precioUnitario)
+tasaImpuestoAplicado = facturacion.calculoTasaImpuestoFijo()
+facturacion.pintarParametros(cantidad, precioUnitario, tasaImpuestoAplicado, facturacionTotal)
 facturacionTotal += facturacionTotal*tasaImpuestoAplicado
 puts "Total: #{facturacionTotal}"
