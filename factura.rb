ec238ef830bbd80424dc25ec55218ad43d55469e
JoelGuimack <you@example.com>
2020-10-23 00:20:36 +0000

    Slice 3, Cálculo de facturación simple(precio*cantidad)

--- factura.rb
+++ factura.rb
@@ -1,3 +1,9 @@
+def precioTotal(cantidad, precioUnitario)
+    return cantidad.to_f * precioUnitario.to_f
+end
+
 cantidad = ARGV[0]
 precioUnitario = ARGV[1]
-puts "cantidad: #{cantidad}, precio unitario: #{precioUnitario}"
+facturacionTotal = precioTotal(cantidad, precioUnitario)
+puts "Cantidad: #{cantidad}, Precio Unitario: #{precioUnitario}"
+puts "Total: #{facturacionTotal}"
