54b5903a191a20e561114d88dcab24fe37e4963b
JoelGuimack <you@example.com>
2020-10-23 00:43:37 +0000

    Slice 6, Impresión de impuesto Fijo

--- factura.rb
+++ factura.rb
@@ -2,18 +2,18 @@ def precioTotal(cantidad, precioUnitario)
     return cantidad.to_f * precioUnitario.to_f
 end
 
-def calculoTasaImpuesto()
+def calculoTasaImpuestoFijo()
     return 0.0825
 end
 
 def pintarParametros(cantidad, precioUnitario, tasaImpuestoAplicado, facturacionTotal)
     puts "Cantidad: #{cantidad}, Precio Unitario: #{precioUnitario} = #{facturacionTotal}"
-    puts "Impuesto Aplicado: #{tasaImpuestoAplicado}"
+    puts "Impuesto Aplicado Fijo: #{tasaImpuestoAplicado}"
 end
 cantidad = ARGV[0]
 precioUnitario = ARGV[1]
 facturacionTotal = precioTotal(cantidad, precioUnitario)
-tasaImpuestoAplicado = calculoTasaImpuesto()
+tasaImpuestoAplicado = calculoTasaImpuestoFijo()
 pintarParametros(cantidad, precioUnitario, tasaImpuestoAplicado, facturacionTotal)
 facturacionTotal += facturacionTotal*tasaImpuestoAplicado
 puts "Total: #{facturacionTotal}"
