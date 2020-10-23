b61b2d3f90a31bfe8faf6e025c1709b8da2f3899
JoelGuimack <you@example.com>
2020-10-23 00:28:40 +0000

    Slice 4, Aplicando un impuesto Fijo

--- factura.rb
+++ factura.rb
@@ -5,5 +5,8 @@ end
 cantidad = ARGV[0]
 precioUnitario = ARGV[1]
 facturacionTotal = precioTotal(cantidad, precioUnitario)
-puts "Cantidad: #{cantidad}, Precio Unitario: #{precioUnitario}"
+tasaImpuestoAplicado = 0.0825
+puts "Cantidad: #{cantidad}, Precio Unitario: #{precioUnitario} = #{facturacionTotal}"
+facturacionTotal += facturacionTotal*tasaImpuestoAplicado
+puts "Impuesto Fijo: #{tasaImpuestoAplicado}"
 puts "Total: #{facturacionTotal}"
